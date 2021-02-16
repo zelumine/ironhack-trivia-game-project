b1f236d103d76cd8d7382f351e085c3ee5614858
Mélusine Rey <zelumine@live.fr>
2021-02-16 19:08:17 +0100

    Add home page and link the home to the quiz request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 // Variables
-let openTriviaUrl = "https://opentdb.com/api.php?amount=20&type=multiple";
+let openTriviaUrl;
 let questionsArr = [];
 let extractedQuestions;
 let arrIndex = 0;
 let score = 0;
+
+// DOM on quiz page
 const answerBtns = document.querySelectorAll(".answer-btn");
 const question = document.querySelector("#question");
 const result = document.querySelector("#result");
@@ -13,16 +15,35 @@ const questionNum = document.querySelector("#question-number");
 let scoreDisplay = document.querySelector("#score");
 let containerDiv = document.querySelector("#container");
 
-const themeBtns = document.querySelectorAll(".theme-btn");
-const difficultyBtns = document.querySelectorAll(".difficulty");
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// Dealing with home page buttons (theme & difficulty)
 let chosenTheme, chosenDifficulty;
+const buttons = document.querySelectorAll('.theme-box button');
 
-themeBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    console.log(btn);
-  })
-})
+const getBtnInfo = event => {
+  chosenTheme = event.target.getAttribute("data-category");
+  chosenDifficulty = event.target.getAttribute("data-level");
+  console.log(chosenTheme);
+  console.log(chosenDifficulty);
+  getUrl(chosenTheme, chosenDifficulty);
+  window.location.replace("./quiz.html");
+}
 
+const getUrl = (theme, difficulty) => {
+  if (theme === null && difficulty === null) openTriviaUrl = `https://opentdb.com/api.php?amount=5&type=multiple`;
+  else if (theme === null) openTriviaUrl = `https://opentdb.com/api.php?amount=5&difficulty=${difficulty}&type=multiple`;
+  else if (difficulty === null) openTriviaUrl = `https://opentdb.com/api.php?amount=5&category=${theme}&type=multiple`;
+  else openTriviaUrl = `https://opentdb.com/api.php?amount=5&category=${theme}&difficulty=${difficulty}&type=multiple`;
+  console.log("URL updated! yay!");
+  console.log(openTriviaUrl);
+  localStorage.setItem("url", openTriviaUrl);
+  return openTriviaUrl;
+}
+
+buttons.forEach(btn => btn.addEventListener('click', getBtnInfo));
+
+// ---------------------------------------------------------------------------------------------------------------------------------
 
 // Get request
 const getOpenTriviaInfo = () => {
@@ -46,10 +67,15 @@ const getOpenTriviaInfo = () => {
     .catch(err => console.log(err));
 };
 
-getOpenTriviaInfo();
+window.addEventListener('load', () => {
+  openTriviaUrl = localStorage.getItem("url");
+  getOpenTriviaInfo();
+});
 // console.log(questionsArr);
 
-// Other functions 
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// Other functions for quiz page
 const displayInfo = arr => {
   theme.textContent = `Theme: ${arr.category}`;
   difficulty.textContent = `Difficulty: ${arr.difficulty}`;
@@ -81,7 +107,11 @@ const getNextQuestion = arr => {
     displayAnswers(arr[arrIndex]);
   }, 1000);
 
-  if (arrIndex === questionsArr.length - 1) displayResult();
+  if (arrIndex === questionsArr.length - 1) {
+    setTimeout(() => { 
+      displayResult();
+    }, 1500);
+  }
 }
 
 const checkAnswer = (event) => {
@@ -111,16 +141,27 @@ const displayResult = () => {
 
   containerDiv.textContent = "There are no more questions! Here is your score";
   replayBtn.textContent = "Play again!";
+  replayBtn.setAttribute("id", "replay-btn");
   newGameBtn.textContent = "Change theme";
+  newGameBtn.setAttribute("id", "new-game-btn");
 
   finalScoreDisplay.textContent = score;
   containerDiv.appendChild(finalScoreDisplay);
+
   containerDiv.appendChild(buttonsDiv);
   buttonsDiv.appendChild(replayBtn);
   buttonsDiv.appendChild(newGameBtn);
+
+  replayBtn.addEventListener("click", reload);
+  newGameBtn.addEventListener("click", backToHomepage);
 }
 
-// Events
+const reload = () => location.reload();
+
+const backToHomepage = () => location.replace("./index.html");
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// Events on quiz page
 answerBtns.forEach(btn => {
   btn.addEventListener("click", checkAnswer);
 });
